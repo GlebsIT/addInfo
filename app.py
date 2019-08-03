294a2e23e616ec2463d1e40b2e39a70a3f04c090
GlebsIT <belgberez@gmail.com>
2019-08-03 16:25:43 +0400

    first commit

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from forms import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello App! We will add some information'
+def main():
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    user = {'username': 'Miguel'}
+    return render_template('forms.html', title='Home', user=user, form=form)
 
 if __name__ == '__main__':
     app.run()
